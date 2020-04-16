36722213f19679012c2cabea5415bdd3e480adfe
unknown <thbusae@gmail.com>
2020-04-16 16:37:48 +0900

    장고 rest API example

---
+++ movies/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import Movie
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
